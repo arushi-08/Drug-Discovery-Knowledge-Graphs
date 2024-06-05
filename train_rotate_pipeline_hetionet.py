eded9c7824025d8702e25893bf6ae024e4369145
Arushi Sharma <40266350+arushi-08@users.noreply.github.com>
2024-06-05 13:35:57 -0400

    Update train_rotate_pipeline_hetionet.py

--- train_rotate_pipeline_hetionet.py
+++ train_rotate_pipeline_hetionet.py
@@ -1,11 +1,15 @@
-import pandas as pd
-
+import os
 import json
+import datetime
+
+import pandas as pd
 import torch
 from pykeen.triples import TriplesFactory
 from pykeen.pipeline import pipeline
 from pykeen.models import RotatE
 # from pykeen.models import save_model
+
+from config import HETIONET_DATA_PATH
 
 # Load the pipelineconfig.json file
 with open('./train_rotate_model_20231118/best_pipeline/pipeline_config.json') as f:
@@ -14,22 +18,19 @@ with open('./train_rotate_model_20231118/best_pipeline/pipeline_config.json') as
 # Create a fresh knowledge graph instance
 # kg = KnowledgeGraph()
 
-HETIONET_DATA_PATH = (
-    '/afs/cs.pitt.edu/usr0/ars539/biology_project/hetionet_data_bidir.txt'
-)
 print("Loading data")
 
 tf = TriplesFactory.from_path(HETIONET_DATA_PATH)
 training, testing, validation = tf.split([.8, .1, .1])
 
 pd.DataFrame(training.mapped_triples.numpy()).to_csv(
-    '/afs/cs.pitt.edu/usr0/ars539/biology_project/hetionet_data_bidir_train.txt', index=False, sep='\t'
+    os.path.join(HETIONET_DATA_PATH, 'hetionet_data_bidir_train.txt'), index=False, sep='\t'
 )
 pd.DataFrame(validation.mapped_triples.numpy()).to_csv(
-    '/afs/cs.pitt.edu/usr0/ars539/biology_project/hetionet_data_bidir_val.txt', index=False, sep='\t'
+    os.path.join(HETIONET_DATA_PATH, 'hetionet_data_bidir_val.txt'), index=False, sep='\t'
 )
 pd.DataFrame(testing.mapped_triples.numpy()).to_csv(
-    '/afs/cs.pitt.edu/usr0/ars539/biology_project/hetionet_data_bidir_test.txt', index=False, sep='\t'
+    os.path.join(HETIONET_DATA_PATH, 'hetionet_data_bidir_test.txt'), index=False, sep='\t'
 )
 print("Starting pipeline")
 
@@ -57,12 +58,12 @@ result = pipeline(
     },
     training_kwargs={
       "batch_size": 1024,
-      "checkpoint_directory": "/afs/cs.pitt.edu/usr0/ars539/.data/pykeen/checkpoints/",
+      "checkpoint_directory": os.path.join(HETIONET_DATA_PATH,"checkpoints"),
       "checkpoint_frequency": 20,
       "checkpoint_on_failure": True,
       "num_epochs": 500
     },
 )
-result.save_to_directory('./train_rotate_model_20231120')
+result.save_to_directory("./train_rotate_model_{datetime.datetime.today().strftime('%Y%m%d')}")
 
 print("Completed Training")
