c854988fd401624073b820116e29d084e5d259aa
Arushi Sharma <40266350+arushi-08@users.noreply.github.com>
2024-06-05 13:33:41 -0400

    Update train_transe_hetionet.py

--- train_transe_hetionet.py
+++ train_transe_hetionet.py
@@ -1,8 +1,11 @@
+import os
+import datetime
+
 from pykeen.triples import TriplesFactory
 from pykeen.pipeline import pipeline
 import torch
 
-import os
+from config import HETIONET_DATA_PATH
 
 # Set the CUDA_VISIBLE_DEVICES environment variable
 os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'  # Use the first and second GPUs
@@ -10,12 +13,9 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'  # Use the first and second GPUs
 
 # from pykeen.constants import PYKEEN_CHECKPOINTS
 # checkpoint = torch.load(PYKEEN_CHECKPOINTS.joinpath(
-#     '/afs/cs.pitt.edu/usr0/ars539/.data/pykeen/checkpoints/best-model-weights-c909159d-4f5e-4d6e-95aa-7d5e6cf6b043.pt'
+#     HETIONET_DATA_PATH
 # )
                             
-HETIONET_DATA_PATH = (
-    '/afs/cs.pitt.edu/usr0/ars539/biology_project/hetionet_data.txt'
-)
 
 tf = TriplesFactory.from_path(HETIONET_DATA_PATH)
 training, testing, validation = tf.split([.8, .1, .1])
@@ -49,4 +49,4 @@ result = pipeline(
         checkpoint_frequency=10,
      ),
 )
-result.save_to_directory('./train_transe_model_20230731')
+result.save_to_directory(f"./train_transe_model_{datetime.datetime.today().strftime('%Y%m%d')}")
